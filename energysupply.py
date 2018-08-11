374281206eeb758adf87d7bc59aa72e39e168be2
V. Fabian T. Mendes C <31230743+fabianmendes@users.noreply.github.com>
2018-08-11 01:21:05 -0400

    Upgrade (v1.0) energysupply.py

--- energysupply.py
+++ energysupply.py
@@ -45,8 +45,9 @@ try:
         # ver especificaciones de la batería!
         nS0 = bool(nS)
         if nS0:
-            if int(nS) != False and (int(nS) >= 1 or int(nS) <= 20): #puede simplificarse solo colocando: 'if float(var):'
-                        # inlcuso, si mal no recuerdo, se puede delimitar los intervalos con un tal 'range' o 'for'.
+            if int(nS) != False and (
+                    int(nS) >= 1 or int(nS) <= 20):  # puede simplificarse solo colocando: 'if float(var):'
+                # inlcuso, si mal no recuerdo, se puede delimitar los intervalos con un tal 'range' o 'for'.
                 nS = int(nS)
                 break
             else:
@@ -75,21 +76,26 @@ try:
             continue
     i = 1
     while i != 0:
-        Wn = input("\n Inserte los Watts (Potencia nominal, o máx) del MOTOR, o los que querrá suministrar-le [####W]= ")
+        Wn = input(
+            "\n\t Inserte los Watts (Potencia nominal, o máx) del MOTOR,"  # cuidado acá, no se pone coma!
+            "\n\t\t\t\t\to los que querrá suministrar-le [####W]= ")       # ya que tratamos no un INPUT.
         if Wn:
             if float(Wn):  # se omite el '!= False' porque se considera y asume que esto signinica que "si es verdad.."
-              Wn = int(abs(float(Wn)))
-              if Wn >= 99999:
-                print("\nVERIFIQUE los Watts escritos, es muy grande.")
-                i = 1
-              else:
-                if Wn >= 100 or Wn < -99:
-                    # hp = float(Wn)/745.7 # está en el finally.                        
-                    i = 0
-                elif Wn >= 1000:
-                  print("Registrado. Tenga en cuenta que para competencias SAE Aerodesign",
-                        "\nuna de las restricciones, es que lleguen <1000w al motor. Continuemos..")
-                i = 0
+                Wn = int(abs(float(Wn)))
+                if Wn >= 99999:
+                    print("\nVERIFIQUE los Watts escritos, es muy grande.")
+                    i = 1
+                else:
+                    if Wn in range(100):
+                        # hp = float(Wn)/745.7 # está en el finally.
+                        print("\nVERIFIQUE los Watts escritos, es muy pequeño (> 100W).")
+                        i = 0
+                    elif Wn >= 1000:
+                        print("Registrado. Tenga en cuenta que, para competencias SAE Aerodesign,",
+                              "\nuna de las restricciones es que lleguen  < 1000 W al motor. Continuemos..")
+                        i = 0
+                    else:
+                        i = 0
             else:
                 print("Por favor, ingrese únicamente en Números.")
                 i = 1
@@ -99,24 +105,26 @@ try:
 
 finally:
     print(
-        "\n\t Batería de", str(nS)+"S (celdas) y", str(mAh), "mAh. Y una capacidad de descarga de", str(C)+"C.",
-        "\n\t Motor brushless trabajando a", str(Wn), "Watts. A lo que es 'igual' o menor que:",
-        str(format(float(Wn)/745.7, ".2f"))+"HP aprox.")
+        "\n\t Batería de", str(nS) + "S (celdas) y", str(mAh), "mAh. Y una capacidad de descarga de", str(C) + "C.",
+        "\n\t Motor brushless con", str(Wn),
+        "Watts necesarios o máx. A lo que es 'igual' o menor que:", str(format(float(Wn) / 745.7, ".2f")) + "HP (aprox.)")
 
-Ao = (mAh / 1000)* C # se comentó porque este Ao es de la batería mismo. Yeah!! Y con los que buscaremos el ESC.
-                  # ; La Capacidad, recordemos que hay dos:
-                  # continua, y de explosición (por unos mSeg, máx POWER).
-                  # y EN REALIDAD ES: # En una hora puede dar ( )mA a una capacidad de descarga de ( )C = Ao
-                  # Listo.
-Ab = (Ao/C)*60  # Ab son los ampere de a batería, por minutos. Los cuales serán usados para medir el T.
+Ao = (mAh / 1000) * C  # se comentó porque este Ao es de la batería mismo. Yeah!! Y con los que buscaremos el ESC.
+# ; La Capacidad, recordemos que hay dos:
+# continua, y de explosición (por unos mSeg, máx POWER).
+# y EN REALIDAD ES: # En una hora puede dar ( )mA a una capacidad de descarga de ( )C = Ao
+# Listo.
+Ab = (Ao / C) * 60  # Ab son los ampere de a batería, por minutos. Los cuales serán usados para medir el T.
 
-print("\t\tAmphere que podría estar pasando por el ESC: Ao = ", str(Ao)+"A, según los datos de la batería. \n\tTenga cuidado en su selección del ESC,"
-      " se recomienda que el máx.A de éste sea mayor o incluso cerca del doble de", Ao, "A .\n")
+print("\t\tAmphere que PODRÍA llegar a pasar por el ESC: Ao = ",
+      str(format(Ao, ".2f")) + "A, según los datos de la batería. \n\tTenga cidado en su selección del ESC;"
+      "\n\t\t\t se recomienda que el máximo amperaje de éste sea mayor a " + str(format(Ao, ".2f")) + "A, o casi el doble.\n")
+Mult = 1000  # Multiplicador para poder aprovechar los decimales, ya que se perderán al convertirse en int().
 
-Vo0 = nS*4.1556  # Voltaje de la batería (Vo) iniciales (t = 0), cargado!
-Vo1 = nS*3.6444  # Voltaje de la batería (Vo) a punto de descargarse ( = 1)
+Vo0 = nS * 4.1867  # 'Vo[1]' Voltaje de la batería (Vo) iniciales (t = 0), cargado!
+Vo1 = nS * 3.6544  # 'Vo[0]' Voltaje de la batería (Vo) a punto de descargarse ( = 1)
 
-Vo = [int(Vo1*100), int(Vo0*100)]  # máx. Voltios en una batería; 3.7v min y 4.2 máx, o inicial.
+Vo = [int(Vo1 * Mult), int(Vo0 * Mult)]  # máx. Voltios en una batería; 3.7v min y 4.2 máx, o inicial.
 A = []  # Ampheres que deberan poder estar pasando (requeridos..) por el ESC según (..por:) el MOTOR!
 
 Tmin = []  # tiempo relativo que será ya en minutos.
@@ -125,12 +133,12 @@ for x in range(2):
     # por ser sólo dos valores en Vo.
 
     # Ao = (mAh / 1000) * C ; no se usará, vea:
-    Ap = Wn/int(Vo[x]/100)  # 100 para contrarrestar los cien que le metí antes y así aprovechar los decimales pertinentes. Prima
+    Ap = Wn / int(Vo[x] / Mult)  # Mult para contrarrestar los cien que le metí antes y así aprovechar los decimales pertinentes. Prima
     # recordemos: W = i * V. Y de ello, con los Watts del motor y los Voltios de la batería damos con el A para nuestro T.
-    # T = Ab / (A * C) ; C es inversamente proporcional a la corriente (en este caso, Ab) de la batería para su tiempo de descarga. 
-      
-    A.append(int(Ap*1000))    
-    # se multiplica por mil (1000) para luego lograr usar los decimales, puesto que las listas solo sportan str() e int().
+    # T = Ab / (A * C) ; C es inversamente proporcional a la corriente (en este caso, Ab) de la batería para su tiempo de descarga.
+
+    A.append(int(Ap * Mult))
+    # se multiplica por Mult para luego lograr usar los decimales, puesto que las listas solo sportan str() e int().
     # añade dicho elem. a la última posición de la lista; AÑADE.
     # An.insert(x, int(AnP))  # añade, inserta en la posición x de la lista.
 
@@ -141,39 +149,39 @@ for x in range(2):
     # Ap es una CONSTANTE, NO ES Amphere; mucho menos. Entonces da.. por 60, los mins de vida.
     # aquí deberá haber un diferencial.. una especie de.
 
-promTmin = (float(Tmin[0])+float(Tmin[1]))/2
+promTmin = ((float(Tmin[0]) + float(Tmin[1])) / (2 * Mult/10))
 # convistiendolos de string a float para poder usar los decimales.. !
 
-promA = (A[0]+A[1])/2000
-                        # Aún no sabia para qué lo iba a usar (deberá ser alguna cuenta promediada, lo cual no cuadra
-                        # ya que no nos conviene hablar acerca de un A promedio puesto que no ocurrirá,
-                        # ese amperaje varía y a muy altos A será en corto tiempo. NO NOS CONVIENE USAR "A promedio".
-                # EDIT: sí nos conviene puesto que A[] está vinculado al Ampheraje que necesitará el motor, por el
-                # hecho de que la batería posee dos voltajes.. uno cuando está cargado y otro cuando está descargándose.
+promA = (A[0] + A[1]) / (2*Mult)
+# Aún no sabia para qué lo iba a usar (deberá ser alguna cuenta promediada, lo cual no cuadra
+# ya que no nos conviene hablar acerca de un A promedio puesto que no ocurrirá,
+# ese amperaje varía y a muy altos A será en corto tiempo. NO NOS CONVIENE USAR "A promedio".
+# EDIT: sí nos conviene puesto que A[] está vinculado al Ampheraje que necesitará el motor, por el
+# hecho de que la batería posee dos voltajes.. uno cuando está cargado y otro cuando está descargándose.
 
-#promV = ((Vo[0]+Vo[1])/200)  # promedio voltios de los de la batería.
-# ^ lo coloqué dentro del while. 
+# promV = ((Vo[0]+Vo[1])/200)  # promedio voltios de los de la batería.
+# ^ lo coloqué dentro del while.
 
 try:
     while True:
-        V = (input("Coloque los Voltios a CONSUMIR [##.0] o los que suministrará constantemente!\n\t ENTER si serán los mismos que proporciona la batería. Ingrese = "))
+        V = (input(
+            "Coloque los Voltios a CONSUMIR [##.0] o los que suministrará constantemente!\n\t ENTER si serán los mismos que proporciona la batería. Ingrese = "))
         Vbool = bool(V)
-        if (Vbool == False):
-            promV = ((Vo[0] + Vo[1]) / 200) 
+        if not Vbool:
+            promV = ((Vo[0] + Vo[1]) / (2 * Mult))
             V = promV  # Voltaje EN PROMEDIO..
 
-            e = "\tSabiendo que sus voltajes fueron proporcionados EN PROMEDIO por la batería: "+str(V)+"V"
+            e = "\tSabiendo que sus voltajes fueron proporcionados EN PROMEDIO por la batería: " + str(format(V, ".2f")) + "V"
             break
         else:
             if float(V):
                 V = abs(float(V))
-                Vo[1] = int(V*100)
-                
-                promV = ((Vo[0] + Vo[1]) / 200)  # promedio voltios de los de la batería.
-                
+                Vo[1] = int(V * Mult)  # inicialmente, por si esta ya 'usada' o cargada config. STORAGE!
+
+                promV = ((Vo[0] + Vo[1]) / (2 * Mult))  # promedio voltios de los de la batería.
+
                 V = promV
-                e = "\t\tSabiendo que su voltaje de alimentación estaría siendo de: "+str(V)+"V"
-                Vo[1] = Vo[1]/100
+                e = "\t\tSabiendo que su voltaje de alimentación estaría siendo de: " + str(format(V, ".2f")) + "V"
 
                 if V > Vo0:
                     print("\n¡Dicho valor supera al que puede otorgar la batería que tiene en uso! Inténtelo de nuevo.")
@@ -184,15 +192,16 @@ try:
                 print("\nPor favor, ingrese únicamente en Números.")
                 continue
 finally:
-    print("\t\nOk, suponemos que el motor estará trabajando o recibiendo, un voltaje de", str(V)+"V, constantemente.")
+    print("\t\nOk, suponemos que el motor estará trabajando o recibiendo, un voltaje de",
+          str(format(Vo[1]/Mult, ".2f")) + "V, inicialmente.")
 
 Wp = V * promA  # Watts (potencia) que usted MANDARÁ a salir, de carga, DE LA BATERÍA.
-                # ..en promedio!
-                # Voltios que proporcionará la batería. Min 18v, max 22~24v,
-                # en este caso será el promedio de no ponerlo el usuario..
-                # esos V están promediados. 
-Wo = [0,1]
-#td = Wp / Wn  # esto no fue cierto... COMÉNTESE! Esta función no servirá de nada.
+# ..en promedio!
+# Voltios que proporcionará la batería. Min 18v, max 22~24v,
+# en este caso será el promedio de no ponerlo el usuario..
+# esos V están promediados.
+Wo = []
+# td = Wp / Wn  # esto no fue cierto... COMÉNTESE! Esta función no servirá de nada.
 # "Idealmente:
 # Carga de la bateria: 12V x 4A = 48W
 # Consumo/hora: 12V * 0.150A = 1.8W ; esta es la corriente (An[x]) que va a consumir el motor.. dando así los Wn.
@@ -200,22 +209,37 @@ Wo = [0,1]
 # Saludos! " [extraído de http://www.todopic.com.ar/foros/index.php?topic=14382.0 el 2 de Ago, 2018. 13:30]
 # tdMin = td * 60  # OJO con EL NOMBRE de LA VARIABLE
 #  ^  ya se hizo más arriba, como 'Tmin'
-Mult = 10000
-for x in range(2):
-  W = float(V[x])*float(A[x]) # RECORDAR divivirlo entre la misma cifra.. (Mult = 10^4)
-  Wo.append(int(W * Mult))
 
-print("Entonces con ello se estima que:\n\t La batería durará, despreciando fugas y demás, idealmente:",
-      format(promTmin, ".2f"), "Mins.")
+for x in range(2):
+    W = float(Vo[x] * A[x]) / pow(Mult, 2)  # RECORDAR que ambos estaban Mult. por 1000.. (Mult = 10^3)
+    Wo.append(str(format(W, ".2f")))
+
+TminImpri = float(Tmin[1]) / (Mult / 10)
+# había que convertir el valor que estaba en la lista 'Tmin[]' porque era str()...
+
+print("Entonces con ello se estima que:\n\t\t La batería durará, despreciando fugas y demás, idealmente: "
+      + str(format(TminImpri, ".2f")) + " Mins. MÁX!")
 # OJO CON el nombre DE LA VARIABLE.
 
-print(e, "\n\t\t..con un Amphere promedio (requeridos por el motor) de", str(format(promA, ".2f"))+"A, \n\t\t y un "
-      "tiempo promedio de descarga, en cuanto a la batería, de", format(promTmin, ".2f"), "Mins.")
-print("\n Hablando de unos", str(format(Wp, ".2f"))+"W en promedio por parte de la batería, y\n\t", str(format(Wo[0]/Mult, ".2f"))+"~"+str(format(Wo[0]/Mult, ".2f"))+"W 'necesarios' o máx. hacia el avión, es decir, \n\tson los que podrían pasar por el Limitador de potencia en algún momento dado!")
+print(e, "\n\t\t..con un Amphere promedio (requeridos por el motor) de",
+      str(format(promA, ".2f")) + "A, \n\t\t y un tiempo promedio de descarga, en cuanto a la batería, de",
+      format(promTmin, ".2f"), "Mins.")
+print("\n Hablando de unos", str(format(Wp, ".2f")) + "W en promedio por parte de la batería, y\n\t",
+      Wo[1] + "~" + Wo[0] + "W 'necesarios' o máx. hacia el avión, es decir, \nson los que podrían pasar por el Limitador de potencia en algún momento dado!")
 
+# EnergySupplyRC v1.0
 # EUPLv1.2, 2018. AEROdesignUCV - #ROADtoMexico'19 !
 # Departamento General de Diseño Eléctrico.Equipo SAE Aerodesign, Universidad Central de Venezuela; Fac. Ingeniería. CCS
 
+# ______________________________________________
 
-#TODO: queda VALIDAR verificar y acomodar muy bien la ecuación para hallar y sacar el tiempo de autonomía, según la batería y motor.
-#TODO hay que ver que esta ocurriendo en la línea 203.
+# TODO: (v .0.1) chequear que al darle ENTER o valores de entrada (input), el programa siga
+# TODO, es decir, como usuario, tratar de ¡dañar el programa! Conseguirle sus 'bugs'.
+# LISTO: queda VALIDAR verificar y acomodar muy bien la ecuación para hallar y sacar el tiempo de autonomía, según la batería y motor.
+
+# TODO:(v .1.0) Chequear y acomodar, de ser el caso,
+# TODO los datos que sobre los Amphere que PODRÍAN pasar por el ESC, según la batería.
+
+# Todo: energysupply + ".py" Next-version (2.0), con las eficiencias y las temperaturas!
+
+# TODO energysupplyRC Version 3.0, ayudarse con mathlab u otro programa! Quizas ya no sea con ".py"
